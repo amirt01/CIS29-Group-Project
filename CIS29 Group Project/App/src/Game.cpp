a81de4e314a9931751a1c43b04ceff1d0ca48b89
amir tadros <amirtadros01@gmail.com>
2020-05-09 00:10:45 -0700

    Basic window settings initialization from config file.

--- CIS29 Group Project/App/src/Game.cpp
+++ CIS29 Group Project/App/src/Game.cpp
@@ -8,7 +8,38 @@ void Game::initVariables()
 
 void Game::initializeWindow()
 {
-	renderWindow = new sf::RenderWindow(sf::VideoMode(800, 600), "CIS29 Group Project");
+    std::ifstream fin("Config/window.txt");
+    
+    /*
+    Game Title
+    render window width _ render window height
+    framerate limit
+    vertical sync enabled
+    */
+    
+    std::string game_title = "DEFAULT";
+    sf::VideoMode render_window_bounds(800, 600);
+    unsigned framerate_limt = 120;
+    bool vertical_sync_enabled = false;
+
+    if (fin.is_open())
+    {
+        std::getline(fin, game_title);
+        fin >> render_window_bounds.width >> render_window_bounds.height;
+        fin >> framerate_limt;
+        fin >> vertical_sync_enabled;
+    }
+    else
+    {
+        std::cout << "Error reading file window setting's file." << std::endl;
+        system("PAUSE");
+    }
+
+    fin.close();
+
+	renderWindow = new sf::RenderWindow(render_window_bounds, game_title);
+    renderWindow->setFramerateLimit(framerate_limt);
+    renderWindow->setVerticalSyncEnabled(vertical_sync_enabled);
 }
 
 // Constructor / Destructors
