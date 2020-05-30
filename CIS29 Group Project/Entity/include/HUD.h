bd12449dcc3bb6da207a86901191e4780d471688
amir tadros <amirtadros01@gmail.com>
2020-05-29 19:58:08 -0700

    Working heart system

--- CIS29 Group Project/Entity/include/HUD.h
+++ CIS29 Group Project/Entity/include/HUD.h
@@ -1,6 +1,24 @@
 #pragma once
+
+#include "Player.h"
+#include "Entity.h"
+
 class HUD
 {
+private:
+	Player* player;
+
+	std::array<Entity, 3> hearts;
+
+	// Initializers
+	void initializeHearts(sf::Texture& heartTexture);
+
+public:
+	HUD(Player* player, sf::Texture& heartTexture);
+	~HUD();
+
+	void update();
+	void render(sf::RenderTarget* renderTarget);
 };
 
 
--- CIS29 Group Project/Entity/src/HUD.cpp
+++ CIS29 Group Project/Entity/src/HUD.cpp
@@ -1,2 +1,34 @@
 #include "stdafx.h"
 #include "HUD.h"
+
+void HUD::initializeHearts(sf::Texture& heartTexture)
+{
+	float position = 0.5;
+	for (Entity& heart : hearts)
+	{
+		heart.setTexture(heartTexture);
+		heart.setPosition(position += 75, 10.f);
+	}
+}
+
+HUD::HUD(Player* player, sf::Texture& heartTexture)
+	: player(player)
+{
+	initializeHearts(heartTexture);
+}
+
+HUD::~HUD()
+{
+}
+
+void HUD::update()
+{
+}
+
+void HUD::render(sf::RenderTarget* renderTarget)
+{
+	for (int i = 0; i < player->getCurrentHealth(); i++)
+	{
+		hearts[i].render(renderTarget);
+	}
+}
