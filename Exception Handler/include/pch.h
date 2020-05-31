788e2b654b4857d04f2fd403444a1b68433d817f
amir tadros <amirtadros01@gmail.com>
2020-05-30 21:24:05 -0700

    Initial setup

---
+++ Exception Handler/include/pch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+//Streams
+#include<iostream>
+#include<fstream>
+#include<sstream>
+
+//Data Structures
+#include<vector>
+#include<stack>
+#include<map>
+
+//Other
+#include<cstdlib>
+#include<ctime>
+
+//SFML Specific
+#include"SFML\System.hpp"
+#include"SFML\Window.hpp"
+#include"SFML\Graphics.hpp"
+#include"SFML\Audio.hpp"
+#include"SFML\Network.hpp"
