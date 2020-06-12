2c6207c8a56391e6ffc00ba3c2fd9be47a1a961d
amir tadros <amirtadros01@gmail.com>
2020-06-11 21:09:12 -0700

    GameStats class

---
+++ CIS29 Group Project/App/src/GameStats.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "GameStats.h"
+
+GameStats::GameStats(long coins, sf::Texture* playerTexture, Themes themes)
+	: coins(coins), playerTexture(playerTexture), themes(themes)
+{
+}
